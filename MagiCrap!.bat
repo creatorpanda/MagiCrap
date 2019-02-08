6a7b5339f76d2a643cf7b41f3cb1e4066e37b06c
creatorpanda <creatorpanda@users.noreply.github.com>
2019-02-08 03:14:08 +0200

    main script upload

---
+++ MagiCrap!.bat
@@ -0,0 +1,18 @@
+@ECHO OFF
+TITLE MagiCrap! -by creatorpanda
+CLS
+COLOR 0d
+ECHO ---------------------------------------------------------
+ECHO A super smart panda will now stop Magicka from working..
+ECHO ---------------------------------------------------------
+ECHO.
+ECHO.
+ECHO RESULTS:
+ECHO -----------------------------------------------------------------------
+TASKKILL /F /IM Magicka.exe
+ECHO -----------------------------------------------------------------------
+ECHO.
+ECHO GitHub: creatorpanda
+ECHO creation of 2019
+ECHO.
+set /p temp="Press ENTER to close this window!"
